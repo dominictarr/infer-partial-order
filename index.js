eeb3baf78f25be4e1d7259a6ffc7a4a68409146e
Dominic Tarr <dominic.tarr@gmail.com>
2014-06-05 18:19:28 +0000

    canonical order

--- index.js
+++ index.js
@@ -9,17 +9,47 @@ function union (a, b) {
   return c.sort()
 }
 
-module.exports = function (examples) {
+function remove(ary, v) {
+  var i = ary.indexOf(v)
+  if(~i) ary.splice(i, 1)
+  return ary
+}
+
+function clean (order) {
+  var newOrder = {}
+  for(var key in order) {
+    var after = order[key]
+
+    //remove all the items that the are indirectly before key.
+    var direct = after.slice()
+    after.forEach(function (k) {
+      order[k].forEach(function (v) {
+        remove(direct, v)
+      })
+    })
+    newOrder[key] = direct
+  }
+  return newOrder
+}
+
+module.exports = function (examples, doClean) {
 
   var order = {}
   examples.forEach(function (seq, i) {
     var seen = []
-    console.log(order)
     seq.forEach(function (value) {
+      console.log(value, '->', order[value] || [])
+      console.log('seen', seen)
       order[value] = union(seen.slice(), order[value])
       seen.push(value)
     })
     if(order) return
   })
+
+  if(doClean)
+    return clean(order)
+
   return order
 }
+
+module.exports.canonical = clean
